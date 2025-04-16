08d2ef87195c5802bf567a4ae81563b9a994cd9c
Ayush9256 <sainiayush.9256@gmail.com>
2025-04-16 13:47:25 +0530

    Add files via upload

---
+++ FAQ/faq.js
@@ -0,0 +1,17 @@
+const outers = document.querySelectorAll('.outer');
+
+outers.forEach(faq => {
+    const icon = faq.querySelector('.icon');
+    const answer = faq.querySelector('.answer');
+
+    faq.addEventListener('click', () => {
+        if(icon.classList.contains('active')) {
+            icon.classList.remove('active');
+            answer.style.maxHeight = null;
+        }else {
+            icon.classList.add('active');
+            answer.style.maxHeight = answer.scrollHeight + 'px';
+        }
+}
+
+)})
